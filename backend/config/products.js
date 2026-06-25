34be645732b2977e35e45ff305fee93bf920c129
Precious115 <francisprecious070@gmail.com>
2026-06-25 01:38:36 +0100

    Update products.js

--- backend/config/products.js
+++ backend/config/products.js
@@ -2,19 +2,19 @@ module.exports = {
   PREMIUM_1_MONTH: {
     key: 'premium_1_month',
     label: 'Premium 1 Month',
-    stars: 100,
+    stars: 500,
     days: 30,
+  },
+  PREMIUM_2_MONTHS: {
+    key: 'premium_2_months',
+    label: 'Premium 2 Months',
+    stars: 1000,
+    days: 60,
   },
   PREMIUM_3_MONTHS: {
     key: 'premium_3_months',
     label: 'Premium 3 Months',
-    stars: 250,
+    stars: 2500,
     days: 90,
-  },
-  PREMIUM_LIFETIME: {
-    key: 'premium_lifetime',
-    label: 'Premium Lifetime',
-    stars: 500,
-    days: null,
   },
 };
